ade84786508a76ebf96094ad33388fd3c4080aec
Ryan Hurley <ryan.hurley1212@gmail.com>
2018-10-08 21:47:16 -0600

    Initial updates

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Breakout
+Classic video game built in Python and with Turtle Graphics python library.
--- playBrickBreaker.py
+++ playBrickBreaker.py
@@ -1,10 +1,6 @@
 """Welcome to Breakout, use this file to play the game!"""
 
-
 from brickbreaker import BrickBreaker
-
-#Going For A- Requirements (92.99% maximum)
-
 
 play_game = BrickBreaker()
 
